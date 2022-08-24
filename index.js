ce897c8f7bcd4628d378b7a68aaa0a487d6ce228
leopoldo109 <tm1453766@gmail.com>
2022-08-24 15:54:19 -0300

    se realizo el backend del home

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const homeController = require('./controller/homeController')
+const app = express()
+const port = 8000
+
+
+app.use( express.static('public'))
+app.set('view engine', 'hbs');
+
+
+app.get('/', homeController.home )
+
+
+app.get('*', homeController.notfound)
+
+
+app.listen(port)
+console.log("Esta ejecutando el server " + port)
+
+//10:15
