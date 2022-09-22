4c810f714aefbf858e7b3720c121dbb61ae19c99
Herrera Franco <69646385+scott1ee@users.noreply.github.com>
2022-09-22 18:33:12 -0300

    actualizacion de script
    actualizacion de la tabla de usuarios
    agregando el campo contraseña
    actualizando todas las tablas con fechas validas en sus updateAt y createdAt respectivos

--- db_scripts/agro_forum.sql
+++ db_scripts/agro_forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 07, 2022 at 05:01 AM
+-- Generation Time: Sep 22, 2022 at 11:30 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -31,7 +31,7 @@ CREATE TABLE `comentarios` (
   `id_comentarios` int(11) NOT NULL,
   `comentario_usuario` varchar(100) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updatedAt` datetime NOT NULL,
+  `updateAt` datetime NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `id_respuesta` int(11) NOT NULL,
   `id_post` int(11) NOT NULL
@@ -41,9 +41,9 @@ CREATE TABLE `comentarios` (
 -- Dumping data for table `comentarios`
 --
 
-INSERT INTO `comentarios` (`id_comentarios`, `comentario_usuario`, `createdAt`, `updatedAt`, `id_usuario`, `id_respuesta`, `id_post`) VALUES
-(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ligula ullamcorper, sagittis le', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
-(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0);
+INSERT INTO `comentarios` (`id_comentarios`, `comentario_usuario`, `createdAt`, `updateAt`, `id_usuario`, `id_respuesta`, `id_post`) VALUES
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ligula ullamcorper, sagittis le', '2022-09-22 21:29:51', '2020-01-01 18:29:50', 0, 0, 0),
+(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', '2022-09-22 21:29:54', '2020-01-01 18:29:53', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -57,16 +57,16 @@ CREATE TABLE `plantas` (
   `tipo_planta` int(50) NOT NULL,
   `foto_planta` varchar(50) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updatedAt` datetime NOT NULL
+  `updateAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `plantas`
 --
 
-INSERT INTO `plantas` (`id_planta`, `nombre_planta`, `tipo_planta`, `foto_planta`, `createdAt`, `updatedAt`) VALUES
-(1, 'nopales', 0, 'fotitoplantauwu.png', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'helecho', 0, 'fotoplantitaowo.png', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `plantas` (`id_planta`, `nombre_planta`, `tipo_planta`, `foto_planta`, `createdAt`, `updateAt`) VALUES
+(1, 'nopales', 0, 'fotitoplantauwu.png', '2022-09-22 21:29:37', '2020-01-01 18:29:36'),
+(2, 'helecho', 0, 'fotoplantitaowo.png', '2022-09-22 21:29:39', '2020-01-01 18:29:38');
 
 -- --------------------------------------------------------
 
@@ -79,7 +79,7 @@ CREATE TABLE `posteos` (
   `contenido_planta` varchar(150) NOT NULL,
   `fecha_publicacion` date NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updatedAt` datetime NOT NULL,
+  `updateAt` datetime NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `id_planta` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -88,9 +88,9 @@ CREATE TABLE `posteos` (
 -- Dumping data for table `posteos`
 --
 
-INSERT INTO `posteos` (`id_post`, `contenido_planta`, `fecha_publicacion`, `createdAt`, `updatedAt`, `id_usuario`, `id_planta`) VALUES
-(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ligula ullamcorper, sagittis lectus et, sagittis dui.', '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
-(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ligula ullamcorper, sagittis lectus et, sagittis dui. Maecenas posuere tempus urn', '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0);
+INSERT INTO `posteos` (`id_post`, `contenido_planta`, `fecha_publicacion`, `createdAt`, `updateAt`, `id_usuario`, `id_planta`) VALUES
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ligula ullamcorper, sagittis lectus et, sagittis dui.', '2020-01-01', '2022-09-22 21:30:07', '2020-01-01 18:29:04', 0, 0),
+(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ligula ullamcorper, sagittis lectus et, sagittis dui. Maecenas posuere tempus urn', '2020-01-01', '2022-09-22 21:30:10', '2020-01-01 18:29:07', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -103,19 +103,20 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
   `nom_usuario` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
   `correo` varchar(50) NOT NULL,
   `telefono` int(11) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updatedAt` datetime NOT NULL
+  `updateAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `nom_usuario`, `correo`, `telefono`, `createdAt`, `updatedAt`) VALUES
-(1, 'Federico', 'Pugliese', 'fedeeP09', 'fedepugli123@gmail.com', 24680846, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'Lionel', 'Alberti', 'liober89', 'lionelelmascapito9000@gmail.com', 13570842, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `nom_usuario`, `password`, `correo`, `telefono`, `createdAt`, `updateAt`) VALUES
+(1, 'Federico', 'Pugliese', 'fedeeP09', '', 'fedepugli123@gmail.com', 24680846, '2022-09-22 21:28:37', '2020-01-01 18:28:35'),
+(2, 'Lionel', 'Alberti', 'liober89', '', 'lionelelmascapito9000@gmail.com', 13570842, '2022-09-22 21:28:40', '2020-01-01 18:28:38');
 
 --
 -- Indexes for dumped tables
