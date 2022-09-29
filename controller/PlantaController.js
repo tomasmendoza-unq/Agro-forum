ab9e075d365e651920fd75c7861c252c6eb87196
leopoldo109 <tm1453766@gmail.com>
2022-09-29 11:21:12 -0300

    -Se realizo el backend del login

--- controller/PlantaController.js
+++ controller/PlantaController.js
@@ -5,7 +5,6 @@ const {plantas} = require('../models/Planta.js')
 
 async function getAll(req, res) {
     console.log("Pidiendo los datos de las plantas");
-
     let Plantas = await plantas.findAll();
     let data = {
         Plantas
--- index.js
+++ index.js
@@ -3,21 +3,43 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const session = require('express-session')
-// const passport= require('passport')
 const port = 8000
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('./db/Connection.js');
+
+const {usuarios} = require('./models/Usuario')
 
 const homeController = require('./controller/homeController')
 
 const loginController = require('./controller/LoginController')
 const registerController = require('./controller/RegisterController')
+const LogoutController = require('./controller/LogoutController')
 
-const UsuarioController = require('./controller/UsuarioController')
+const PerfilController = require('./controller/PerfilController')
 const PlantasController = require('./controller/PlantaController')
 const PosteosController = require('./controller/PosteoController')
 const ComentariosController = require('./controller/ComentarioController')
 
 
 //configuracion
+// app.use((req, res, next) => {
+//   if (!req.session.user) {
+//       res.clearSession()     
+//   }
+//   next();
+// });
+
+// var hbsContent = {userName: '', loggedin: false, title: "You are not logged in today", body: "Hello World"}; 
+
+// middleware function to check for logged-in users
+// var sessionChecker = (req, res, next) => {
+//   if (req.session.user) {
+  
+//       res.redirect('/');
+//   } else {
+//       next();
+//   }    
+// };
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
@@ -27,26 +49,54 @@ app.use(session({
   saveUninitialized: true,
 }))
 
-
 //
 
 app.use(express.urlencoded({extended: false}));
+
+
 
 //redirecciones
 
 app.get('/', homeController.home);
 
-app.get('/login', loginController.login);
+
+
+// app.get('/login', async (req, res) => {
+//   if (req.session.user) {
+//   const datos = req.body;
+//   let user = await usuarios.findOne({ where: { correo: datos.email} });
+//   hbsContent.loggedin = true; 
+//   hbsContent.userName = req.session.user; 
+//       res.render('/', hbsContent);
+//   } else {
+//       res.render('login');
+//   }
+// });
+
+// app.get('/logout', (req, res) => {
+//   if (req.session.user) {
+//   hbsContent.loggedin = false; 
+//   hbsContent.title = "You are logged out!"; 
+//       res.clearSession('user_sid'); 
+//       res.redirect('/');
+//   } else {
+//       res.redirect('/login');
+//   }
+// });
+
+app.get('/login', loginController.login)
 
 app.post('/login', registerController.register);
 
-app.get('/usuario', UsuarioController.getAll);
+app.post('/', loginController.logeo);
 
 app.get('/plantas', PlantasController.getAll);
 
+app.get('/perfil', PerfilController.getAll)
+
 app.get('/posteos', PosteosController.getAll);
 
-app.get('/comentarios', ComentariosController.getAll);
+app.get('/logout', LogoutController.logout);
 
 app.get('*', homeController.notfound)
 
