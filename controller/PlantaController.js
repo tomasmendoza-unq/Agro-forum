a1dd85f3349d4a6cb3feb7a25ac592868d17980f
scott1ee <franhe345@gmail.com>
2022-11-09 21:11:20 -0300

    creacion de planta y edicion

--- controller/PlantaController.js
+++ controller/PlantaController.js
@@ -1,17 +1,53 @@
-const { Model, DataTypes,} = require('sequelize');
+const { Model, DataTypes, } = require('sequelize');
 const sequelize = require('../db/Connection.js');
 
-const {plantas} = require('../models/Planta.js')
+const { plantas } = require('../models/Planta.js')
+/*
+- Creacion de plantas
+- Edicion de plantas
+- Visualizacion de todas las plantas
+- Eliminacion de plantas
+- Ver una planta
+*/
+async function create(req, res) {
+    let data = req.body
 
-async function getAll(req, res) {
-    console.log("Pidiendo los datos de las plantas");
-    let Plantas = await plantas.findAll();
-    let data = {
-        Plantas
-    }   
-    res.render('plantas',data)
+    const posteoPlanta = await posteos.create(
+        {
+            nombre_planta: data.nombre_planta,
+            tipo_planta: data.tipo_planta,
+            foto_planta: data.foto_planta
+        });
+
+    res.render('posteoPlanta', { posteoPlanta })
+}
+
+async function edit(req, res) {
+    let data = req.body
+
+    let editPlanta = await edits.findByPk(data.id);
+
+    if (editPlanta) {
+        let edit = await editPlanta.update(
+            {
+                nombre_planta: data.nombre_planta,
+                tipo_planta: data.tipo_planta,
+                foto_planta: data.foto_planta
+            },
+            {
+                where: { id_post: data.id },
+            }
+        )
+
+        let editPlants = await edits.findAll()
+
+        res.render(`editar`, { editPlants })
+    } else {
+        res.send('error')
+    }
 }
 
 module.exports = {
-    getAll
+    create,
+    edit,
 };
